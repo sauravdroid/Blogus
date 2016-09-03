6a2e86dc1702470a98ab3cc2af3ab75e4a6524d8
sauravbiswas <saru.sreyo@gmail.com>
2016-09-03 16:56:22 +0530

    Sixth Commit

--- Blogus/settings.py
+++ Blogus/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'User.apps.UserConfig',
+    'Post.apps.PostConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
